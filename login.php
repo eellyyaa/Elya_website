46fb32156ad772da807510d03af10fe012144af9
eellyyaa <148841245+eellyyaa@users.noreply.github.com>
2025-01-03 23:46:51 +1000

    login.php

--- login.php
+++ login.php
@@ -19,7 +19,7 @@ if (isset($_POST['submit'])) {
     }
 
 
-    $link = mysqli_connect('127.0.0.1', 'root', 'kali', 'Elya_website');
+    $link = mysqli_connect('127.0.0.1', 'root', 'kali', 'first');
     if (!$link) {
         die("Ошибка подключения: " . mysqli_connect_error());
     }
